3f4a0247f82ad1cca28ddbb16a85a157ce9f9914
agenziastraordinaria <cloud@agenziastraordinaria.com>
2026-03-10 21:06:32 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById("travel-map-root");
+
+if (container) {
+  ReactDOM.createRoot(container).render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>
+  );
+}
